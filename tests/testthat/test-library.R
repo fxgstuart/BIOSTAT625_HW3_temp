36668005269c64a5e71d4deda4a1f707ba19c69c
Xiangeng Fang <xgfang@umich.edu>
2022-11-21 16:48:37 -0500

    Modified test

--- tests/testthat/test-library.R
+++ tests/testthat/test-library.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+fit <- lm(hp~mpg+disp+drat+wt+qsec, data = mtcars)
+X <- as.matrix(mtcars[,c("mpg","disp","drat","wt","qsec")])
+y <- as.vector(mtcars[,"hp"])
+
+test_that("fit.coef works", {
+  expect_equal(fit.coef(X,y), unname(fit$coefficients))
 })
+
+
