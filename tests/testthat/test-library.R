298b4b0a0007d1167286e6773ab429a09c551ebf
Xiangeng Fang <xgfang@umich.edu>
2022-11-21 22:14:28 -0500

    Modified man Modified test

--- tests/testthat/test-library.R
+++ tests/testthat/test-library.R
@@ -11,54 +11,54 @@ test_that("fit.coef works", {
   expect_equal(fit.coef(X,y), unname(fit$coefficients))
 })
 
-test_that("fit.values works"){
+test_that("fit.values works",{
   expect_equal(fit.values(X,y),unname(fit$fitted.values))
-}
+})
 
-test_that("fit.predict works"){
+test_that("fit.predict works",{
   expect_equal(fit.predict(Xtrain,ytrain,Xtest),unname(predict.lm(fit2,Xtest)))
-}
+})
 
-test_that("fit.sigma.squared works"){
+test_that("fit.sigma.squared works",{
   expect_equal(sqrt(fit.sigma.squared(X,y)),unname(tb$sigma))
-}
+})
 
-test_that("fit.sd works"){
+test_that("fit.sd works",{
   expect_equal(unname(fit.sd(X,y)),unname(tb$coefficients[,2]))
-}
+})
 
-test_that("fit.t.test works"){
+test_that("fit.t.test works",{
   expect_equal(unname(cbind(fit.t.test(X,y)$t,fit.t.test(X,y)$p.value)),unname(tb$coefficients[,c(3,4)]))
-}
+})
 
-test_that("fit.confint works"){
+test_that("fit.confint works",{
   expect_equal(unname(fit.confint(X,y)),unname(confint(fit)))
-}
+})
 
-test_that("fit.hat.matrix works"){
+test_that("fit.hat.matrix works",{
   expect_equal(unname(diag(fit.hat.matrix(X))),unname(influence(fit)$hat))
-}
+})
 
-test_that("fit.overall.test works"){
+test_that("fit.overall.test works",{
   res <- fit.overall.test(X,y)
   res_vec <- c(res$F,res$df1,res$df2)
   expect_equal(unname(res_vec),unname(tb$fstatistic))
-}
+})
 
-test_that("fit.R.squared works"){
+test_that("fit.R.squared works",{
   res <- fit.R.squared(X,y)
   res_vec <- c(res$R.squared,res$adj.R.squared)
   expect_equal(unname(res_vec),unname(c(tb$r.squared,tb$adj.r.squared)))
-}
+})
 
-test_that("fit.partial.test works"){
+test_that("fit.partial.test works",{
   res <- fit.partial.test(X,y,idx = c(2))
   res_vec <- c(res$F.stat,res$p.value)
   true_table <- car::Anova(fit,type="III")
   expect_equal(unname(res_vec),unname(c(true_table$`F value`[3],true_table$`Pr(>F)`[3])))
-}
+})
 
-test_that("fit.GLH.test works"){
+test_that("fit.GLH.test works",{
   T.matrix <- matrix(0,nrow=2,ncol=6)
   T.matrix[1,3]<-1
   T.matrix[1,4]<--1
@@ -68,4 +68,4 @@ test_that("fit.GLH.test works"){
   res_vec <- c(res$F.stat,res$p.value)
   true_table <- car::linearHypothesis(model=fit,hypothesis.matrix=T.matrix,rhs=c(0,0))
   expect_equal(unname(res_vec),unname(c(true_table$F[2],true_table$`Pr(>F)`[2])))
-}
+})
